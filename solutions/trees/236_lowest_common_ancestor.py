9f2f27a14a863a1fee7da1f81d60bcfe33e87c07
Rishab Bhat <r1shabbhatkc@gmail.com>
2026-03-26 20:17:41 -0500

    236

--- solutions/trees/236_lowest_common_ancestor.py
+++ solutions/trees/236_lowest_common_ancestor.py
@@ -1,15 +1,32 @@
 """
-Problem: [Title]
-Link: https://leetcode.com/problems/[slug]/
-Difficulty: [Easy/Medium/Hard]
+Problem: Lowest Common Ancestor of a Binary Tree
+Link: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/description/
+Difficulty: medium
 
 Approach:
-- 
+we use post order traversal to return up the values if we find 
+p and q. if they come from both left and right
+then we know the current node is the lca
+if not then we just return up whatever came in
 
-Time Complexity: O()
-Space Complexity: O()
+Time Complexity: O(log(n))
+Space Complexity: O(h) where h is the hight of the tree.
 """
 
 
 class Solution:
-    pass
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        
+        def dfs(node):
+            if not node or node == p or node == q:
+                return node
+            
+            left = dfs(node.left)
+            right = dfs(node.right)
+
+            if left and right:
+                return node
+
+            return left if left else right
+        
+        return dfs(root)
