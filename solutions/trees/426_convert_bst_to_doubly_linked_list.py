68a229aa0952fc9ea110e0d1701108ba7e2a2abf
Rishab Bhat <r1shabbhatkc@gmail.com>
2026-03-28 15:25:48 -0500

    lc 426

--- solutions/trees/426_convert_bst_to_doubly_linked_list.py
+++ solutions/trees/426_convert_bst_to_doubly_linked_list.py
@@ -1,15 +1,50 @@
 """
-Problem: [Title]
-Link: https://leetcode.com/problems/[slug]/
-Difficulty: [Easy/Medium/Hard]
+Problem: Convert Binary Search Tree to Sorted Doubly Linked List
+Link: https://leetcode.com/problems/convert-binary-search-tree-to-sorted-doubly-linked-list/description/
+Difficulty: medium
 
-Approach:
+Approach: use inorder traversal to string together the DLL
 - 
 
-Time Complexity: O()
-Space Complexity: O()
+Time Complexity: O(n)
+Space Complexity: O(h) - because of recursive stack
+o(logn) for balanced tree, o(n) for skewed
 """
 
 
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+"""
+
 class Solution:
-    pass
+    def treeToDoublyList(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        if not root:
+            return
+        
+        self.first = None
+        self.last = None
+
+        def dfs(node):
+            if not node:
+                return
+            
+            dfs(node.left)
+            if self.last:
+                self.last.right = node
+                node.left = self.last
+            else:
+                self.first = node
+            self.last = node
+            dfs(node.right)
+
+        
+        dfs(root)
+        self.first.left = self.last
+        self.last.right = self.first
+        return self.first
+        
