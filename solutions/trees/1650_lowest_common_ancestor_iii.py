c02594cef01132826d61b140006117187f4cf317
Rishab Bhat <r1shabbhatkc@gmail.com>
2026-03-27 22:45:30 -0500

    lc 1650

--- solutions/trees/1650_lowest_common_ancestor_iii.py
+++ solutions/trees/1650_lowest_common_ancestor_iii.py
@@ -11,5 +11,23 @@ Space Complexity: O()
 """
 
 
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+        self.parent = None
+"""
+
 class Solution:
-    pass
+    def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
+        # currP = A currQ = A
+        currP, currQ = p, q
+
+        while currP != currQ:
+            currP = q if not currP else currP.parent
+            currQ = p if not currQ else currQ.parent
+
+        return currP
