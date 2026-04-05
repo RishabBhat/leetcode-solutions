e737a0f5d60611cd5b32191fc1362c704ac8182f
Rishab Bhat <r1shabbhatkc@gmail.com>
2026-04-04 22:52:45 -0500

    correct lru cache

--- solutions/design/146_lru_cache.py
+++ solutions/design/146_lru_cache.py
@@ -1,69 +1,58 @@
-"""
-Problem: LRU Cache
-Link: https://leetcode.com/problems/lru-cache/
-Difficulty: Medium
-
-Approach:
-Use a doubly linked list + hashmap. The list maintains recency order
-(head = most recent, tail = least recent). The hashmap provides O(1)
-key lookup. On get/put, move the accessed node to the head. On capacity
-overflow, evict the tail node.
-
-Time Complexity: O(1) for both get and put
-Space Complexity: O(capacity)
-"""
-
-
 class Node:
-    def __init__(self, key=0, val=0):
+    def __init__(self, key, value):
         self.key = key
-        self.val = val
-        self.prev = None
+        self.val = value
         self.next = None
-
-
+        self.prev = None
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.capacity = capacity
-        self.cache = {}  # key -> node
-        # Sentinel head (most recent) and tail (least recent)
-        self.head = Node()
-        self.tail = Node()
-        self.head.next = self.tail
-        self.tail.prev = self.head
-
-    def _remove(self, node: Node) -> None:
-        node.prev.next = node.next
-        node.next.prev = node.prev
-
-    def _insert_front(self, node: Node) -> None:
-        node.next = self.head.next
-        node.prev = self.head
-        self.head.next.prev = node
-        self.head.next = node
+        self.cap = capacity
+        self.cache = {}
+        self.left = Node(0, 0)
+        self.right = Node(0, 0)
+        self.left.next = self.right
+        self.right.prev = self.left
+        
 
     def get(self, key: int) -> int:
-        if key not in self.cache:
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.add(self.cache[key])
+            return self.cache[key].val
+        else:
             return -1
-        node = self.cache[key]
-        self._remove(node)
-        self._insert_front(node)
-        return node.val
+        
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
-            self._remove(self.cache[key])
+            self.remove(self.cache[key])
         node = Node(key, value)
+        self.add(node)
         self.cache[key] = node
-        self._insert_front(node)
-        if len(self.cache) > self.capacity:
-            lru = self.tail.prev
-            self._remove(lru)
-            del self.cache[lru.key]
+
+        if len(self.cache) > self.cap:
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key] 
+
+    def add(self, node):
+        nxt = self.right
+        prev = nxt.prev
+        prev.next = node
+        node.prev = prev
+        nxt.prev = node
+        node.next = nxt
+        
+    def remove(self, node):
+        prev = node.prev
+        nxt = node.next
+        prev.next = nxt
+        nxt.prev = prev
+
 
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
-# obj.put(key, value)
+# obj.put(key,value)
